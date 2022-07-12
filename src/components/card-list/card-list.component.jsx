1d23c7167528ac656bb36c3fcb77729ca5bba96e
Amol shelke <shelkeamol897@gmail.com>
2022-07-12 14:19:04 +0530

    Creating Card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './card-list.style.css';
 
-export const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
-};
+export const CardList = (props) => (
+  <div className="card-list">
+    {props.monsters.map((monster) => (
+      <h1 key={monster.id}> {monster.name} </h1>
+    ))}
+  </div>
+);
